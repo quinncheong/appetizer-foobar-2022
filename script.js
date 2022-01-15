5ff8d9379904c46382193fca74c0b14fb65ca390
Quinn Cheong <quinncheong@hotmail.com>
2022-01-15 09:05:50 +0800

    Setting up the project

---
+++ script.js
@@ -0,0 +1,27 @@
+// returns a string containing sentences of length 10 max
+function cleanText(text) {
+    textCleaned = text.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ").toLowerCase();
+    textArray = textCleaned.split(' ');
+    let result = '';
+    let tmp = [];
+    for (word of textArray){
+        if (tmp.length < 10){
+            tmp.push(word);
+        } else {
+            sentence = tmp.join(' ') + '. '
+            result += sentence.charAt(0).toUpperCase() + sentence.slice(1);
+            tmp = [];
+        }
+    }
+    if (tmp != []){
+        sentence = tmp.join(' ') + '. '
+        result += sentence.charAt(0).toUpperCase() + sentence.slice(1);
+    }
+    console.log(result);
+    return result
+}
+
+function createVideo(userInput){
+    let data = cleanText(userInput);
+    // call api
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require("express");
+const cors = require("cors");
+const path = require("path");
+const helmet = require("helmet"); // For security
+
+// Initialise the express app
+const app = express();
+app.use(helmet());
+app.use(cors());
+
+// Body-parser middleware usage for req.body
+app.use(express.json({ limit: "50mb" }));
+
+//Other Routes
+// app.use("/api/database", require("./routes/api/database"));
+// app.use("/api/realtime", require("./routes/api/realtime"));
+
+// All code below only to be used in production
+if (process.env.NODE_ENV === "production") {
+	// set static folder
+	app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+	app.get("/*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
+app.get("/", (req, res) => {
+	res.send("The Foobar Fighter's server is up and running!");
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () =>
+	console.log(
+		`Server stared on port ${PORT}, NODE_ENV is ${process.env.NODE_ENV}`
+	)
+);
