6ee9e60ddb53d12926cb3b10e15590325d322b19
Quinn Cheong <quinncheong@hotmail.com>
2022-01-16 22:59:03 +0800

    Add landing page beauty things

--- client/src/components/Landing.vue
+++ client/src/components/Landing.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="hello">
+  <div class="landing-wrapper">
+    <header class="landing-header">
+      <nav class="links text-white" v-if="fullView">
+        <template :key="route.path" v-for="(route, index) in routes">
+          <router-link v-if="route.meta.visible" :to="route.path.toLowerCase()">
+            {{ route.name }}
+          </router-link>
+        </template>
+      </nav>
+
+      <!-- If on small screen -->
+      <nav class="nav small" v-else>
+        <div id="nav-icon3" :class="classNames" @click="showButton">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <div id="home-dropdown-small" :class="dropdownClass" @click="showButton">
+          <!-- Router link to other paths -->
+          <template :key="route.path" v-for="(route, index) in routes">
+            <router-link v-if="route.meta.visible" :to="route.path.toLowerCase()">
+              {{ route.name }}
+            </router-link>
+          </template>
+        </div>
+      </nav>
+
+      <div class="header-middle-text d-flex">
+        <h4 class="text-white">Save My World,</h4>
+        <h4 class="text-white">Together</h4>
+      </div>
+
+      <a @click="$refs.main.$el.scrollIntoView()" class="header-arrow">
+        <span></span>
+      </a>
+    </header>
+
     <h1>{{ msg }}</h1>
     <p>Built for teachers, by Students.</p>
     <button @click="handleClick" class="btn btn-success">Get Video Url</button>
@@ -58,6 +96,57 @@ export default {
     transform: rotate(360deg);
   }
 }
+
+.landing-wrapper {
+  .landing-header {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    flex-direction: column;
+    max-height: 100vh;
+    padding: 0.5rem;
+    background-image: url("~@/assets/teaching2.jpg");
+    background-position: center; /* Center the image */
+    background-repeat: no-repeat; /* Do not repeat the image */
+    background-size: cover; /* Resize the background image to cover the entire container */
+    position: relative;
+
+    .header-middle-text {
+      background-color: rgba(0, 0, 0, 0.5);
+      margin-top: 15rem;
+      padding: 2rem;
+      width: 20px;
+      width: 70%;
+      flex-direction: column;
+      align-items: flex-start;
+    }
+
+    .links {
+      display: flex;
+      width: 90%;
+      justify-content: space-around;
+    }
+
+    .header-arrow {
+      margin-top: auto;
+      margin-bottom: 30px;
+      cursor: pointer;
+      animation: 2.5s infinite alternate bouncer;
+
+      span {
+        display: block;
+        width: 32px;
+        height: 32px;
+        border-right: 2px solid;
+        border-bottom: 2px solid;
+        border-color: #fefefe;
+        -webkit-transform: rotate(45deg);
+        transform: rotate(45deg);
+      }
+    }
+  }
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -71,5 +160,16 @@ li {
 }
 a {
   color: #42b983;
+}
+
+/* For safari */
+@keyframes bouncer {
+  50% {
+    transform: translateY(-16px);
+  }
+
+  100% {
+    transform: translateY(0px);
+  }
 }
 </style>
